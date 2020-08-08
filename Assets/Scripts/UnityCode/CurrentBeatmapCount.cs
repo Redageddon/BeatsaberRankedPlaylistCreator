ed34ddf0db00595a200d790eedf7d736910b93a6
Rubiksmaster02 <therealrubiksmaster@gmail.com>
2020-08-08 14:15:58 -0700

    removed defualt uselesess

--- Assets/Scripts/UnityCode/CurrentBeatmapCount.cs
+++ Assets/Scripts/UnityCode/CurrentBeatmapCount.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using UnityEngine;
 
@@ -11,21 +10,20 @@ namespace UnityCode
     {
         public static int CurrentMapCount { get; private set; }
 
-        private async void Start()
+        private void Start()
         {
             if (CurrentMapCount == 0)
             {
-                CurrentMapCount = await GetCurrentBeatmapCount();
+                CurrentMapCount = GetCurrentBeatmapCount();
             }
         }
         
-        private static async Task<int> GetCurrentBeatmapCount()
+        private static int GetCurrentBeatmapCount()
         {
             using WebClient webClient = new WebClient();
 
             string allData =
-                await webClient.DownloadStringTaskAsync(
-                    "http://scoresaber.com/api.php?function=get-leaderboards&page=1&limit=10000&unique=1&ranked=1");
+                webClient.DownloadString("http://scoresaber.com/api.php?function=get-leaderboards&page=1&limit=10000&unique=1&ranked=1");
 
             return (JObject.Parse(allData)["songs"] ?? throw new NullReferenceException()).Count();
         }
